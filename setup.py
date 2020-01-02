b83b43cae72fcb08bc4106b1546efef7e7f620c4
JTProg <mail@jtprog.ru>
2020-01-03 01:58:34 +0300

    Add deploy to PyPI;

--- setup.py
+++ setup.py
@@ -3,27 +3,28 @@
 """Setup module for NDenv"""
 
 
-from setuptools import setup
+import setuptools
 
-try: # fix nose error
-    import multiprocessing
-except ImportError:
-    pass
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-setup(name='ndenv',
-      version=__import__('ndenv').__version__,
-      description='Handle .env files',
-      author='Mihael Savin',
-      author_email='mail@jtprog.ru',
-      url='https://github.com/jtprog/ndenv',
-      test_suite='nose.collector',
-      packages=setuptools.find_packages(),
-      scripts=['scripts/ndenv'],
-      classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-      ],
-      python_requires='>=3.6',
-      )
+setuptools.setup(
+    name='ndenv',
+    version=__import__('ndenv').__version__,
+    description='Handle .env files',
+    author='Mihael Savin',
+    author_email='mail@jtprog.ru',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/jtprog/ndenv',
+    test_suite='nose.collector',
+    packages=setuptools.find_packages(),
+    scripts=['scripts/ndenv'],
+    classifiers=[
+      "Programming Language :: Python :: 3",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
 
